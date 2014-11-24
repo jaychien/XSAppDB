6017212ede24b6d32d62b93d5da81fb648c9959d
derektu <derektu123@gmail.com>
2014-11-24 15:30:40 +0800

    add importtrigger.html: 提供手動轉檔

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var AppDB = require('./lib/appdb.js');
 var MarketServer = require('./lib/marketserver.js');
+var TriggerImportSvc = require('./lib/triggerimportsvc.js');
 var options = require('./lib/config.json');
 var WebServer = require('./lib/webserver.js');
 
@@ -12,8 +13,11 @@ options.port = options.port || 7001;
 options.mongodb = options.mongodb || "mongodb://127.0.0.1/XSAppDB";
 options.marketserver = options.marketserver || "http://203.67.19.128";
 options.env = options.env || '';
+options.triggerimport = options.triggerimport || {};
+    options.triggerimport.server = options.triggerimport.server || "127.0.0.1";
 
 var marketServer = new MarketServer(options.marketserver);
 var appDB = new AppDB(options.mongodb, marketServer);
+var triggerImportSvc = new TriggerImportSvc(appDB, options.triggerimport);
 
-new WebServer(appDB, marketServer, options.port, options.env).run();
+new WebServer(appDB, marketServer, triggerImportSvc, options.port, options.env).run();
